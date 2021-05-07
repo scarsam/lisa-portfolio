39770affad21b21be5bf41d2a3db69916152354c
sam <s.ojling@gmail.com>
2021-05-07 09:12:22 -0700

    Revalidating props after 30s

--- pages/[project].tsx
+++ pages/[project].tsx
@@ -24,6 +24,7 @@ export async function getStaticProps({ params }) {
       title,
       project,
     },
+    revalidate: 30,
   };
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -12,6 +12,7 @@ export async function getStaticProps() {
     props: {
       projects,
     },
+    revalidate: 30,
   };
 }
 
