033830e794d5276e02a1e7c15ab0c330a848d829
sam <s.ojling@gmail.com>
2021-05-07 09:08:49 -0700

    Removed old page

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,6 @@ export async function getStaticProps() {
 }
 
 export default function Home({ projects }) {
-  console.log(projects);
   const arrowStyles: CSSProperties = {
     position: "absolute",
     zIndex: 2,
